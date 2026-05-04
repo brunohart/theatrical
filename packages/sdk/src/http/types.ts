399bbddcf59e32b619e03d16c044fa40b331cf06
Bruno Hart <123848709+brunohart@users.noreply.github.com>
2026-05-04 14:00:00 +1200

    feat(sdk): export query params, headers, and URL builder from http barrel

--- packages/sdk/src/http/types.ts
+++ packages/sdk/src/http/types.ts
@@ -5,3 +5,7 @@ export type { RateLimiterConfig } from './rate-limiter';
 export { DEFAULT_RATE_LIMITER_CONFIG, RateLimiter } from './rate-limiter';
 export type { RequestInterceptor, ResponseInterceptor, RequestConfig } from './interceptors';
 export { runInterceptors } from './interceptors';
+
+export { serializeQueryParams, parseQueryString, type QueryValue } from './query-params';
+export { createDefaultHeaders, type RequestHeaders } from './headers';
+export { UrlBuilder } from './url-builder';
